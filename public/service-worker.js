5b7570c4bcd062202dfaad5c13e4ddc002e5a6b8
JasonDesktop <MaJason93@gmail.com>
2021-09-26 12:58:47 -0700

    This is a variation of service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,3 @@
-
 const FILES_TO_CACHE = [
  "/",
  "/index.html",
@@ -14,54 +13,68 @@ const FILES_TO_CACHE = [
  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
-  );
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// TODO: add listener and handler to retrieve static assets from the Cache Storage in the browser 
+// install
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+        return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+    caches.keys().then(keyList => {
+    return Promise.all(
+        keyList.map(key => {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+        console.log("Removing old cache data", key);
+         return caches.delete(key);
+        }
+        })
         );
-      })
-      .then(() => self.clients.claim())
-  );
+    })
+    );
+
+    self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
+
+self.addEventListener("fetch", function (evt) {
+    // cache successful requests to the API
+    if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+    caches.open(DATA_CACHE_NAME).then(cache => {
+      return fetch(evt.request)
+        .then(response => {
+        // Check for response..
+        if (response.status === 200) {
+        cache.put(evt.request.url, response.clone());
         }
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
+    // Checking for event response.
+    evt.respondWith(
+    caches.match(evt.request).then(function (response) {
+    return response || fetch(evt.request);
+        })
     );
-  }
 });
